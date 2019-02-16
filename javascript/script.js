924ad4639e16dcdd7784090209f5f9a3ed5d94bf
Jerry L Hoover <jerry.hoover@ymail.com>
2019-02-15 21:16:53 -0600

    Changes in Portfolio Landing Page

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+// This is the script for my portfolio landing page
+
+//TODO
+
+//Create random fun fact 
+//generate random number between 1 and # of fun facts
+//read selected fun fact
+//inject selected fun fact into DOM
+console.log("WORKS");
