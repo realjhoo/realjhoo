45919936a559c077ace03876f462d5a1a850caf7
Jerry L Hoover <jerry.hoover@ymail.com>
2019-02-17 21:01:20 -0600

    Added favicon and language pngs

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,20 @@
-// This is the script for my portfolio landing page
+// This script is copyrigh (c) 2019
+// Jerry L Hoover - Two Brave Dogs
 
 //TODO
+    //Create random fun fact 
+    //generate random number between 1 and # of fun facts
+    //read selected fun fact
+    //inject selected fun fact into DOM
 
-//Create random fun fact 
-//generate random number between 1 and # of fun facts
-//read selected fun fact
-//inject selected fun fact into DOM
-console.log("WORKS");
+// =-=-=-=-= This function creates a random number =-=-=-=-=-=
+function getFunFactIndex(min, max) { 
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var funFactIndex = 0;
+
+funFactIndex = getFunFactIndex(0, 100);
+
+// TEMP DEBUG OUTPUT
+console.log(funFactIndex);
